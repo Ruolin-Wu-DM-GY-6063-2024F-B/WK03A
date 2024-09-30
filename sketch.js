117b1aeb86d2fdd6e11126f0e54fbe54b4fea7b7
fuyuookura <fuyuookura@163.com>
2024-09-30 01:17:30 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,21 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(233,212,0)
 }
 
 function draw() {
+  for (let x = 0; x < windowWidth; x += 50) {
+    for (let y = 0; y < windowHeight; y += 50) {
+      
+      if ((x + y) % 100 === 0) {  
+        fill(0);
+        noStroke();
+        ellipse(x, y, 25); 
+      } else {
+        fill(0);
+        noStroke();
+        ellipse(x, y, 10); 
+      }
+    }
+  }
 }
